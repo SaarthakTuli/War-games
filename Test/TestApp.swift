458f0a0e8467821c40755f16cf196e1dc7d0e1d4
Saarthak <Saarthaktuli@gmail.com>
2021-08-16 20:16:20 +0530

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Saarthak Tuli on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
